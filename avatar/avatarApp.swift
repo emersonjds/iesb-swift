c457f35dfc3bed09abd4a0b8f69a02d6866ae41f
Emerson Silva <emerson_jdss@hotmail.com>
2021-12-21 20:47:03 -0300

    Initial Commit

---
+++ avatar/avatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  avatarApp.swift
+//  avatar
+//
+//  Created by Emerson Silva on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct avatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
